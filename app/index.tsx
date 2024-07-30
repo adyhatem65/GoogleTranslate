ce236906f6cce3d8c658ca9841b2056122369c0a
adyhatem65 <adyetman65@gmail.com>
2024-07-30 15:19:26 +0300

    manage state

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,18 @@
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { Stack } from 'expo-router';
+import { useState } from 'react';
 import { Text, TextInput, View } from 'react-native';
 
 export default function Home() {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+
+  const onTranslate = () => {
+    const translation = input;
+    setOutput(translation);
+  };
+
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
@@ -19,6 +28,8 @@ export default function Home() {
         <View className="flex-row gap-5">
           {/* Input */}
           <TextInput
+            value={input}
+            onChangeText={setInput}
             placeholder="Enter your text"
             className="min-h-32 flex-1 text-xl"
             multiline
@@ -26,23 +37,30 @@ export default function Home() {
             textAlignVertical="top"
           />
           {/* send icon */}
-          <FontAwesome6 name="circle-arrow-right" size={24} color="royalblue" />
+          <FontAwesome6
+            onPress={onTranslate}
+            name="circle-arrow-right"
+            size={24}
+            color="royalblue"
+          />
         </View>
         {/* mic and letters view */}
         <View className="flex-row justify-between">
           <FontAwesome6 name="microphone" size={18} color="dimgray" />
-          <Text className="color-gray-500">0 / 5000</Text>
+          <Text className="color-gray-500">{input?.length || 0} / 5000</Text>
         </View>
       </View>
 
       {/* Output Container */}
-      <View className="gap-5 bg-gray-200 p-5">
-        <Text className="min-h-32 text-xl">Output</Text>
-        <View className="flex-row justify-between">
-          <FontAwesome6 name="volume-high" size={18} color="dimgray" />
-          <FontAwesome5 name="copy" size={18} color="dimgray" />
+      {output && (
+        <View className="gap-5 bg-gray-200 p-5">
+          <Text className="min-h-32 text-xl">Output</Text>
+          <View className="flex-row justify-between">
+            <FontAwesome6 name="volume-high" size={18} color="dimgray" />
+            <FontAwesome5 name="copy" size={18} color="dimgray" />
+          </View>
         </View>
-      </View>
+      )}
     </>
   );
 }
