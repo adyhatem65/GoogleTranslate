4580708261b2f21ee4c9ce03bbe912390735576b
adyhatem65 <adyetman65@gmail.com>
2024-07-30 14:57:46 +0300

    translation UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,48 @@
-import { Stack, Link } from 'expo-router';
-
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { Stack } from 'expo-router';
+import { Text, TextInput, View } from 'react-native';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+
+      {/* Langauge Selector Row */}
+      <View className="flex-row items-center justify-around p-5 ">
+        <Text className="font-semibold color-blue-600">English</Text>
+        <FontAwesome5 name="exchange-alt" size={16} color="gray" />
+        <Text className="font-semibold color-blue-600">Spanish</Text>
+      </View>
+      {/* Input Container */}
+      <View className="border-y border-gray-300 p-5">
+        <View className="flex-row gap-5">
+          {/* Input */}
+          <TextInput
+            placeholder="Enter your text"
+            className="min-h-32 flex-1 text-xl"
+            multiline
+            maxLength={5000}
+            textAlignVertical="top"
+          />
+          {/* send icon */}
+          <FontAwesome6 name="circle-arrow-right" size={24} color="royalblue" />
+        </View>
+        {/* mic and letters view */}
+        <View className="flex-row justify-between">
+          <FontAwesome6 name="microphone" size={18} color="dimgray" />
+          <Text className="color-gray-500">0 / 5000</Text>
+        </View>
+      </View>
+
+      {/* Output Container */}
+      <View className="gap-5 bg-gray-200 p-5">
+        <Text className="min-h-32 text-xl">Output</Text>
+        <View className="flex-row justify-between">
+          <FontAwesome6 name="volume-high" size={18} color="dimgray" />
+          <FontAwesome5 name="copy" size={18} color="dimgray" />
+        </View>
+      </View>
     </>
   );
 }
